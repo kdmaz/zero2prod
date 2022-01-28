6046bf29411926c502256bdbe48f2217e1b91fe3
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-01-28 11:10:51 -0500

    avoids unnecessary memory allocation

--- src/email_client.rs
+++ src/email_client.rs
@@ -34,11 +34,11 @@ impl EmailClient {
         text_content: &str,
     ) -> Result<(), reqwest::Error> {
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
         let url = Url::parse(self.base_url.as_str()).unwrap();
         let url = url.join("email").unwrap();
@@ -57,12 +57,12 @@ impl EmailClient {
 
 #[derive(Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 #[cfg(test)]
