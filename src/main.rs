2c8b587029c7f9720bb61aa0a1522b3c6b677703
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-01-28 09:00:37 -0500

    hooks up empty email client to app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use sqlx::postgres::PgPoolOptions;
+use zero2prod::email_client::EmailClient;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -14,11 +15,13 @@ async fn main() -> std::io::Result<()> {
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
 
+    let sender_email = configuration.email_client.sender().expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, pool)?.await?;
+    run(listener, pool, email_client)?.await?;
     Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,7 @@ use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use zero2prod::email_client::EmailClient;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -35,7 +36,13 @@ async fn spawn_app() -> TestApp {
     configuration.database.database_name = Uuid::new_v4().to_string();
     let pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, pool.clone()).expect("Failed to bind address");
+    let sender_email = configuration.email_client.sender().expect("Invalid sender email address");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email
+    );
+
+    let server = run(listener, pool.clone(), email_client).expect("Failed to bind address");
     tokio::spawn(server);
     TestApp { address, pool }
 }
