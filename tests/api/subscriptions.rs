7af7c5a1be3f451f823338d7d2b47a955cebcd28
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-01-28 15:49:58 -0500

    new subscriber set to pending

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -17,15 +17,25 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await;
 
     let response = app.post_subscriptions(body.into()).await;
-    assert_eq!(200, response.status().as_u16());
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
         .fetch_one(&app.pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
