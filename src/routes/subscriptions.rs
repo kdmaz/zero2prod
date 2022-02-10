85d24fe6ccbd76feec65b40775a1390083fa37f9
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-02-09 19:51:20 -0500

    implements naive approach for newsletter

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -28,7 +28,7 @@ impl TryFrom<FormData> for NewSubscriber {
     }
 }
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
@@ -176,7 +176,7 @@ pub async fn send_confirmation_email(
     );
 
     email_client
-        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .send_email(&new_subscriber.email, "Welcome!", &html_body, &plain_body)
         .await
 }
 
