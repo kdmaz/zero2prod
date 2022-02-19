eaf9bb53264008dfd8c18f4fab9b3c2dd48e78f9
kyle <kyle.mazeika@gmail.com>
2022-02-19 11:06:27 -0500

    adds login routes

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
@@ -80,6 +82,8 @@ fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
