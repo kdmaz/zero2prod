4a5007866fef46a82bb1c37268916c750c53919c
kyle <kyle.mazeika@gmail.com>
2022-02-20 15:02:39 -0500

    adds watch script

---
+++ scripts/watch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v cargo)" ]; then
+  echo >&2 "Error: `cargo` is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v cargo-watch)" ]; then
+  echo >&2 "Error: `cargo-watch` is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v bunyan)" ]; then
+  echo >&2 "Error: `bunyan` is not installed."
+  exit 1
+fi
+
+cargo watch -x check -x test -x "run | bunyan"
