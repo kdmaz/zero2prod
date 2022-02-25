ff02bd8a56d5404bbcb64d8992d057d474b49e46
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-02-25 10:56:20 -0500

    cargo fmt

--- src/routes/admin/logout.rs
+++ src/routes/admin/logout.rs
@@ -4,9 +4,10 @@ use actix_web::HttpResponse;
 use actix_web_flash_messages::FlashMessage;
 
 pub async fn log_out(session: TypedSession) -> Result<HttpResponse, actix_web::Error> {
-	if session.get_user_id().map_err(e500)?.is_none() {
-		Ok(see_other("/login"))
-	} else {
-		FlashMessage::info("You have successfully logged out.").send(); Ok(see_other("/login"))
-	}
+    if session.get_user_id().map_err(e500)?.is_none() {
+        Ok(see_other("/login"))
+    } else {
+        FlashMessage::info("You have successfully logged out.").send();
+        Ok(see_other("/login"))
+    }
 }
